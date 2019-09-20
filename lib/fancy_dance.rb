cac1e237f6bb621b08464d16b80462056d63f363
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-20 18:47:27 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,8 +1,27 @@
-require_relative "../lib/kid.rb"
-require_relative "../lib/dancer.rb"
+module FancyDance
+  module InstanceMethods
  
-angelina = Kid.new("Angelina")
-mikhail_baryshnikov = Dancer.new("Mikhail")
+    def twirl
+      "I'm twirling!"
+    end
  
-puts "#{angelina.name} says: #{angelina.twirl}"
-puts "#{mikhail_baryshnikov.name} says: #{mikhail_baryshnikov.take_a_bow}"
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+ 
+  module ClassMethods
+ 
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+end
